b6054a55a88bc530ff2362238c0622204f11c39d
barbarabrito <barbaramabrito@gmail.com>
2022-04-10 19:40:33 -0300

    fix- pages

--- src/pages/Vaquinha/index.jsx
+++ src/pages/Vaquinha/index.jsx
@@ -8,7 +8,7 @@ function Vaquinha() {
             <h1 id="title-vaquinha">Vaquinha</h1>
             <br/>
             <div className="vaquinha-container">
-                <p>Com o advento da Pandemia do novo corona virús a comunidade Kiriri vem passando por diversas problemáticas.</p>
+                <p>Com o advento da Pandemia do novo coronavírus a comunidade Kiriri vem passando por diversas problemáticas.</p>
                 <p>Agora com a retomada da economia se tornou necessário reformar e reorganizar as ações e produções no território.</p>
                 <p>Sendo assim, queremos pedir a contribuição de vocês para nossa Vaquinha, que ajudará a comunidade no conserto de seus maquinários para novamente produzir seus produtos como farinha,
                     fécula de mandioca, beiju e outros.</p>
